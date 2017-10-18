3a608d7adc5713ddcf659b33b5e61d8b8b29dcb3
lufubinGit <lufubin@147hotmail.com>
2017-10-18 11:10:01 +0800

    1

---
+++ BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  Constellation
+//
+//  Created by JD on 2017/10/18.
+//  Copyright © 2017年 JD. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
